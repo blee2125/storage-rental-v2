f907cbc2e627209aade63ef1a0b991ae01238530
Brandon Lee <blee2125@gmail.com>
2022-11-18 19:31:07 -0700

    update to link from href

--- src/components/View/NavBar.js
+++ src/components/View/NavBar.js
@@ -2,27 +2,24 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
 
 function NavBar() {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Storage Rental</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">Storage Units</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
             <NavDropdown title="Customer" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/customers">Customers</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Add Customer
-              </NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/customers">Customers</NavDropdown.Item>
             </NavDropdown>
             <NavDropdown title="Storage Units" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/storage-units">Storage Units</NavDropdown.Item>
+              <NavDropdown.Item as={Link} to="/storage-units">Storage Units</NavDropdown.Item>
               <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
+              <NavDropdown.Item as={Link} to="/storage-units/add">
                 Add Storage Unit
               </NavDropdown.Item>
             </NavDropdown>
