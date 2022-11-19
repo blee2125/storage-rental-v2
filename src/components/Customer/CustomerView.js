6beaa18553d2232a40bbbeac258d3ba401050a6f
Brandon Lee <blee2125@gmail.com>
2022-11-19 13:50:12 -0700

    import lease list

--- src/components/Customer/CustomerView.js
+++ src/components/Customer/CustomerView.js
@@ -2,12 +2,13 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
-
+import LeaseList from './List/LeaseList'
 
 function CustomerView(props) {
     let navigate = useNavigate();
     const routeParams = useParams();
     const customer = useSelector((state) => state.customerState.customerArray.filter(c => c._id === routeParams.id)[0])
+    const leases = useSelector((state) => state.leaseState.leaseArray.filter(l => l.customerId === routeParams.id))
 
     function editCustomer() {
         let path = `../edit/${customer._id}`
@@ -42,9 +43,21 @@ function CustomerView(props) {
                             {customer.phone}
                         </td>
                     </tr>
+                    <tr>
+                        <td>
+                            # of Leases
+                        </td>
+                        <td>
+                            {leases.length}
+                        </td>
+                    </tr>
                 </tbody>
             </table>
             <button onClick={editCustomer}>edit</button>
+
+            <LeaseList
+                leases={leases}
+            />
         </div>
     )
 }
