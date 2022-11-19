c7dd6a9e4cb1a7df21db52b4090a336b45e84386
Brandon Lee <blee2125@gmail.com>
2022-11-19 16:31:01 -0700

    started filters for leases

--- src/components/Lease/LeaseForms/LeaseStorageUnitForm.js
+++ src/components/Lease/LeaseForms/LeaseStorageUnitForm.js
@@ -3,11 +3,34 @@ import { Form } from "react-bootstrap";
 
 export const LeaseStorageUnitForm = (props) => {
 
+  const listLocations = () => {
+    const locations = []
+    props.units.forEach((unit) => {
+      locations.push(unit.location)
+    })
+    function onlyUnique(value, index, self) {
+      return self.indexOf(value) === index;
+    }
+    const filteredLoc = locations.filter(onlyUnique)
+    return(filteredLoc)
+  }
+
     const listStorageUnits = props.units.map(unit => {
             return <option value={unit._id} key={unit._id}>{unit.unitNumber}</option>
         })
 
     return (
+      <div>
+        <Form.Group className="mb-3" controlId="formGroupLocation">
+          <Form.Label>Location</Form.Label>
+          <Form.Select
+            value={props.location || ''}
+            onChange={e => props.updateData('unitId', e.target.value)}
+          >
+            <option value=''></option>
+            {listStorageUnits}
+          </Form.Select>
+        </Form.Group>
         <Form.Group className="mb-3" controlId="formGroupUnit">
           <Form.Label>Unit</Form.Label>
           <Form.Select
@@ -18,6 +41,8 @@ export const LeaseStorageUnitForm = (props) => {
             {listStorageUnits}
           </Form.Select>
         </Form.Group>
+        {listLocations()}
+      </div>
     );
 }
 
