d2bc95ee97324104adc2da484466ed8250e9986b
Brandon Lee <blee2125@gmail.com>
2022-11-19 10:20:34 -0700

    create customer form field

---
+++ src/components/Lease/LeaseForms/LeaseCustomerForm.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Form } from "react-bootstrap";
+
+export const LeaseCustomerForm = (props) => {
+
+    const listCustomers = props.customers.map(customer => {
+            return <option value={customer._id} key={customer._id}>{customer.customerName}</option>
+        })
+
+    return (
+        <Form.Group className="mb-3" controlId="formGroupCustomer">
+          <Form.Label>Customer</Form.Label>
+          <Form.Select
+            value={props.customerId || ''}
+            onChange={e => props.updateData('customerId', e.target.value)}
+          >
+            <option value=''></option>
+            {listCustomers}
+          </Form.Select>
+        </Form.Group>
+    );
+}
+
+export default LeaseCustomerForm;
