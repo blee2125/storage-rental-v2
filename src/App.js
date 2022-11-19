8cc53f780f91783356eaa03cfe3c4b7567235e07
Brandon Lee <blee2125@gmail.com>
2022-11-18 18:25:58 -0700

    added NavBar and BrowserRouter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Provider } from 'react-redux'
 import store from './store'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+import NavBar from './components/View/NavBar';
 import Customer from './components/Customer/Customer'
 import StorageUnit from './components/StorageUnit/StorageUnit'
 
@@ -10,8 +12,13 @@ function App() {
   return (
     <div className="App">
       <Provider store={store}>
-        <Customer/>
-        <StorageUnit/>
+        <NavBar/>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/customers" element={<Customer/>}/>
+            <Route path="/storage-units" element={<StorageUnit/>}/>
+          </Routes>
+        </BrowserRouter>
       </Provider>
     </div>
   );
