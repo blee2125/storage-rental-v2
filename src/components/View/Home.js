f758cfbef1ad5b978e07b61b5233b608dc587e53
Brandon Lee <blee2125@gmail.com>
2022-11-18 19:30:41 -0700

    create home route component

---
+++ src/components/View/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { connect } from "react-redux";
+
+function Home(props) {
+
+    return (
+        <div>
+            home
+        </div>
+    )
+}
+
+export default connect(null, null) (Home)
