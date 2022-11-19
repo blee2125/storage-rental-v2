d666fa129fe9df626e9dcba4646545975a947183
Brandon Lee <blee2125@gmail.com>
2022-11-19 07:49:17 -0700

    table rows are clickable to view objects

--- src/components/Customer/List/CustomerListItem.js
+++ src/components/Customer/List/CustomerListItem.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function CustomerListItem(props) {
+    let navigate = useNavigate();
+
+    const viewCustomer = () => {
+        const customerToView = props.customer
+        let path = `view/${props.customer._id}`
+        navigate(path, {state: {customerToView}})
+    }
 
     return (
-        <tr>
+        <tr onClick={()=> viewCustomer()}>
             <td>{props.customer.customerName}</td>
         </tr>
     );
--- src/components/StorageUnit/List/StorageUnitListItem.js
+++ src/components/StorageUnit/List/StorageUnitListItem.js
@@ -1,9 +1,16 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function StorageUnitListItem(props) {
+    let navigate = useNavigate();
+
+    const viewStorageUnit = () => {
+        let path = `view/${props.storageUnit._id}`
+        navigate(path)
+    }
 
     return (
-        <tr>
+        <tr onClick={()=> viewStorageUnit()}>
             <td>{props.storageUnit.unitNumber}</td>
         </tr>
     );
