0b0d8690d79f9c4646537f5a2fd9e0e3ee7e119e
Brandon Lee <blee2125@gmail.com>
2022-11-23 20:11:02 -0700

    import stat components

--- src/components/View/Home.js
+++ src/components/View/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
+import UnitStats from "../DataStats/UnitStats";
+import CustomerStats from "../DataStats/CustomerStats";
 
 function Home(props) {
 
     return (
         <div>
             home
+            <UnitStats />
+            <CustomerStats />
         </div>
     )
 }
