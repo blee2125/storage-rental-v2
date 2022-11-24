348a5a405dd1fa896c70f7154aeb88398bc706e3
Brandon Lee <blee2125@gmail.com>
2022-11-24 11:22:34 -0700

    calculate total cost, navigate to lease

--- src/components/Lease/LeaseForms/LeaseAdd.js
+++ src/components/Lease/LeaseForms/LeaseAdd.js
@@ -3,11 +3,10 @@ import { Button } from "react-bootstrap"
 import { connect, useSelector } from "react-redux";
 import { createLease } from "../../../reducers/LeaseReducer"
 import LeaseForm from "./LeaseForm";
-import { getAllStorageUnits } from '../../../reducers/StorageUnitReducer'
-import { getAllCustomers } from '../../../reducers/CustomerReducer'
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function LeaseAdd(props) {
+    let navigate = useNavigate();
     const {state} = useLocation();
     const id = state;
     const customers = useSelector((state) => state.customerState.customerArray)
@@ -16,6 +15,7 @@ function LeaseAdd(props) {
         unitId: '',
         customerId: '',
         rate: '',
+        totalCost: '',
         startDate: '',
         endDate: ''
     });
@@ -26,7 +26,7 @@ function LeaseAdd(props) {
         setLeaseObject(leaseObject => ({
             ...leaseObject,
             ...updatedValue
-        }));        
+        }));
     };
 
     const handleSubmit = () => {
@@ -34,6 +34,7 @@ function LeaseAdd(props) {
         .unwrap()
         .then((data) => {
             console.log(data)
+            navigate(`/leases/view/${data._id}`)
         })
         .catch((e) => {console.log(e)});
     }
@@ -44,9 +45,45 @@ function LeaseAdd(props) {
         }
     }
 
+    const daysInMonth = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+
+    const calcTotal = () => {
+        const startSplit = leaseObject.startDate.split('-')
+        const startYear = Number(startSplit[0])
+        const startMonth = Number(startSplit[1])
+        const startDay = Number(startSplit[2])
+        const endSplit = leaseObject.endDate.split('-')
+        const endYear = Number(endSplit[0])
+        const endMonth = Number(endSplit[1])
+        const endDay = Number(endSplit[2])
+        let m = 0
+        let d = 0
+        const yearDiff = (endYear - startYear) *12
+        const monthDiff = endMonth - startMonth
+        const dayDiff = endDay - startDay
+
+        m = yearDiff + monthDiff
+        if (dayDiff < 0) {
+            if (startYear%4 === 0) {
+                m = m - 1
+                if (startMonth === 2) {
+                    d = daysInMonth[startMonth] + dayDiff + 1
+                } else {
+                    d = daysInMonth[startMonth] + dayDiff
+                }
+            } else {
+                m = m - 1
+                d = daysInMonth[startMonth] + dayDiff
+            }
+        } else {
+            d = dayDiff
+        }
+        let total = 0
+        total = (m * leaseObject.rate) + (d * leaseObject.rate / 30)
+        leaseObject.totalCost = Number(total).toFixed(2)
+    }
+
     useEffect(() => {
-        props.getAllStorageUnits()
-        props.getAllCustomers()
         importKeyValue()
         // eslint-disable-next-line
     }, [])
@@ -59,10 +96,11 @@ function LeaseAdd(props) {
                 units={units}
                 leaseObject={leaseObject}
                 updateData={updateData}
+                calcTotal={calcTotal}
             />
             <Button onClick={handleSubmit}>Submit</Button>
         </div>
     )
 }
 
-export default connect(null, { createLease, getAllCustomers, getAllStorageUnits }) (LeaseAdd)
+export default connect(null, { createLease }) (LeaseAdd)
