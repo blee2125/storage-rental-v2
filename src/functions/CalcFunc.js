db2e6d072c252dcf8d482a8e62624f41d80aa5f3
Brandon Lee <blee2125@gmail.com>
2022-11-25 10:32:42 -0700

    create to store common functions

---
+++ src/functions/CalcFunc.js
@@ -0,0 +1,47 @@
+const daysInMonth = [0,31,28,31,30,31,30,31,31,30,31,30,31]
+
+class CalcFunc {
+    
+    // calculates total cost - dates: '2022-01-01' rate: '1.23'(monthly)
+    calcTotal = (startDate, endDate, rate) => {
+        const startSplit = startDate.split('-')
+        const startYear = Number(startSplit[0])
+        const startMonth = Number(startSplit[1])
+        const startDay = Number(startSplit[2])
+        const endSplit = endDate.split('-')
+        const endYear = Number(endSplit[0])
+        const endMonth = Number(endSplit[1])
+        const endDay = Number(endSplit[2])
+        let totalMonths = 0
+        let totaldays = 0
+        const yearDiff = (endYear - startYear) *12
+        const monthDiff = endMonth - startMonth
+        const dayDiff = endDay - startDay
+
+        totalMonths = yearDiff + monthDiff
+        if (dayDiff < 0) {
+            if (startYear%4 === 0) {
+                totalMonths = totalMonths - 1
+                if (startMonth === 2) {
+                    totaldays = daysInMonth[startMonth] + dayDiff + 1
+                } else {
+                    totaldays = daysInMonth[startMonth] + dayDiff
+                }
+            } else {
+                totalMonths = totalMonths - 1
+                totaldays = daysInMonth[startMonth] + dayDiff
+            }
+        } else {
+            totaldays = dayDiff
+        }
+        let total = 0
+        total = (totalMonths * rate) + (totaldays * rate / 30)
+        const totalCost = Number(total).toFixed(2)
+        return totalCost
+    }
+
+
+
+}
+
+export default new CalcFunc()
