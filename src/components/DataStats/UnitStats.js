a27af0d3739b1520fcf426fea0394061b1815ae8
Brandon Lee <blee2125@gmail.com>
2022-11-27 15:59:17 -0700

    update display info

--- src/components/DataStats/UnitStats.js
+++ src/components/DataStats/UnitStats.js
@@ -1,25 +1,30 @@
 import React from "react";
 import { useSelector } from "react-redux";
+import DateFunc from "../../functions/DateFunc";
+import { Card } from 'react-bootstrap';
 
 function UnitStats() {
     const units = useSelector((state) => state.storageUnitState.storageUnitArray)
-    let totalLeases = 0
-    let totalAvailable = 0
+    const leases = useSelector((state) => state.leaseState.leaseArray)
+    let currentLeases = 0
+    let futureLeases = 0
 
-    units.forEach(unit => {
-        if (unit.available) {
-            totalAvailable = totalAvailable + 1
-        } else {
-            totalLeases = totalLeases + 1
+    leases.forEach(l => {
+        const pcf = DateFunc.returnPastCurrentFuture(l.startDate, l.endDate)
+        if (pcf === 'Current') {
+            currentLeases = currentLeases + 1
+        } else if (pcf === 'Future') {
+            futureLeases = futureLeases + 1
         }
-    });
+    })
 
     return (
         <div>
-            unit stats
+            <Card bg='light' border="secondary" style={{ width: '300px', padding: '25px', margin: "25px"}}>
             <p>Total Units - {units.length}</p>
-            <p>Total Leases - {totalLeases}</p>
-            <p>Total Available - {totalAvailable}</p>
+            <p>Current Leases - {currentLeases}</p>
+            <p>Upcoming Leases - {futureLeases}</p>
+            </Card>
         </div>
     )
 }
