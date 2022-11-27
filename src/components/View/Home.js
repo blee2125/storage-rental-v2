34278f749e70e5c121bd00dcb10e44dea2ec8d4d
Brandon Lee <blee2125@gmail.com>
2022-11-27 15:58:24 -0700

    add pie chart

--- src/components/View/Home.js
+++ src/components/View/Home.js
@@ -2,14 +2,20 @@ import React from "react";
 import { connect } from "react-redux";
 import UnitStats from "../DataStats/UnitStats";
 import CustomerStats from "../DataStats/CustomerStats";
+import LeasePieChart from "../DataStats/LeasePieChart";
+import { Col, Row } from 'react-bootstrap';
 
 function Home(props) {
 
     return (
         <div>
-            home
-            <UnitStats />
+            <Row>
+                <Col lg='auto'><UnitStats /></Col>
+                <Col lg='auto'><LeasePieChart /> </Col>
+            </Row>
+            
             <CustomerStats />
+
         </div>
     )
 }
