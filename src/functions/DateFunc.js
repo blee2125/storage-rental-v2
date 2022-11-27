81289cbfe97dccdaf12d6b2317af0b3fe25de0ed
Brandon Lee <blee2125@gmail.com>
2022-11-27 14:58:10 -0700

    refactor and fix bug

--- src/functions/DateFunc.js
+++ src/functions/DateFunc.js
@@ -48,38 +48,15 @@ class DateFunc {
 
     // returns past, current, or future in comparison to today's date
     returnPastCurrentFuture = (startDate, endDate) => {
-        const startSplit = startDate.split('-')
-        const startYear = Number(startSplit[0])
-        const startMonth = Number(startSplit[1])
-        const startDay = Number(startSplit[2])
-        const endSplit = endDate.split('-')
-        const endYear = Number(endSplit[0])
-        const endMonth = Number(endSplit[1])
-        const endDay = Number(endSplit[2])
         const today = new Date()
-        const todayYear = today.getFullYear()
-        const todayMonth = today.getMonth() + 1
-        const todayDay = today.getDate()
-        if (endYear < todayYear) {
-            return 'Past' //past year
-        }  else if (startYear > todayYear) {
-            return 'Future' //fut year
-        } else {
-            if (endMonth < todayMonth) {
-                return 'Past' //cur year, past month
-            } else if (endMonth === todayMonth) {
-                if (endDay < todayDay) {
-                    return 'Past' //cur year, cur month, past day
-                }
-            }
-            if (startMonth > todayMonth) {
-                return 'Future' //fut month
-            } else if (startMonth === todayMonth) {
-                if (startDay > todayDay) {
-                    return 'Future' //cur month, fut day
-                }
-            }
+        const start = new Date(startDate)
+        const end = new Date(endDate)
+        if (end < today) {
+            return 'Past'
+        } else if (start <= today) {
             return 'Current'
+        } else {
+            return 'Future'
         }
     }
 
