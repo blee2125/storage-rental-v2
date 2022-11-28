607515079fde11e361d4ba39762adfe856ba001c
Brandon Lee <blee2125@gmail.com>
2022-11-27 18:17:27 -0700

    create payment list components

---
+++ src/components/Payment/List/PaymentList.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Table, Card } from "react-bootstrap";
+import PaymentListItem from "./PaymentListItem";
+
+function PaymentList(props) { 
+    const listPayments = props.paymentArray.map((payment, index) => {
+        return (
+            <PaymentListItem 
+                payment={payment} 
+                key={index} 
+                id={payment._id} 
+            />
+        )
+    })
+
+    return (
+        <div>
+            <Card bg='light' border="secondary" style={{ padding: '25px', margin: "25px"}}>
+            <Table hover>
+                <thead>
+                    <tr>
+                        <td><b>lease id</b></td>
+                        <td><b>customer</b></td>
+                        <td><b>payment</b></td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {listPayments}
+                </tbody>
+            </Table>
+            </Card>
+        </div>
+    );
+}
+
+export default PaymentList;
---
+++ src/components/Payment/List/PaymentListItem.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+function PaymentListItem(props) {
+    const customer = useSelector((state) => state.customerState.customerArray.filter(c => c._id === props.payment.customerId)[0])
+    const lease = useSelector((state) => state.leaseState.leaseArray.filter(l => l._id === props.payment.leaseId)[0])
+    let navigate = useNavigate();
+
+    const viewLease = () => {
+        let path = `../../leases/view/${props.payment.leaseId}`
+        navigate(path)
+    }
+
+    return (
+        <tr >
+            <td onClick={()=> viewLease()}>{lease ? lease._id : ''}</td>
+            <td>{customer ? customer.name : ''}</td>
+            <td>{props.payment.payment}</td>
+        </tr>
+    );
+}
+
+export default (PaymentListItem);
