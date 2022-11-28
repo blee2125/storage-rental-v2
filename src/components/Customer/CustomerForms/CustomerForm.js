a12904cfcbd0e4c02e26d15f89a1acb6ff582041
Brandon Lee <blee2125@gmail.com>
2022-11-27 20:11:35 -0700

    add format for phone numbers

--- src/components/Customer/CustomerForms/CustomerForm.js
+++ src/components/Customer/CustomerForms/CustomerForm.js
@@ -1,39 +1,40 @@
 import React from "react";
 import { Form } from "react-bootstrap";
+import PhoneInput from 'react-phone-input-2'
 
 export const CustomerForm = (props) => {
 
-    return (
-      <Form >
-        <Form.Group className="mb-3" controlId="formGroupName">
-          <Form.Label>Name</Form.Label>
-          <Form.Control 
-            type="text" 
-            placeholder="Name" 
-            value={props.customerObject.name}
-            onChange={e => props.updateData('name', e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formGroupEmail">
-          <Form.Label>Email</Form.Label>
-          <Form.Control 
-            type="text" 
-            placeholder="Email" 
-            value={props.customerObject.email}
-            onChange={e => props.updateData('email', e.target.value)}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formGroupPhone">
-          <Form.Label>Phone</Form.Label>
-          <Form.Control 
-            type="text" 
-            placeholder="Phone" 
-            value={props.customerObject.phone}
-            onChange={e => props.updateData('phone', e.target.value)}
-          />
-        </Form.Group>
-      </Form>
-    );
+  return (
+    <Form >
+      <Form.Group className="mb-3" controlId="formGroupName">
+        <Form.Label>Name</Form.Label>
+        <Form.Control 
+          type="text" 
+          placeholder="Name" 
+          value={props.customerObject.name}
+          onChange={e => props.updateData('name', e.target.value)}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formGroupEmail">
+        <Form.Label>Email</Form.Label>
+        <Form.Control 
+          type="text" 
+          placeholder="Email" 
+          value={props.customerObject.email}
+          onChange={e => props.updateData('email', e.target.value)}
+        />
+      </Form.Group>
+      <Form.Group className="mb-3" controlId="formGroupPhone">
+        <Form.Label>Phone</Form.Label>
+        <PhoneInput
+          disableCountryCode={true}
+          specialLabel=''
+          value={props.customerObject.phone}
+          onChange={phone => props.updateData('phone', phone)}
+        />
+      </Form.Group>
+    </Form>
+  );
 }
 
 export default CustomerForm;
