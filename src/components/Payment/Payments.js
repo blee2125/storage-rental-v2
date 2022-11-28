74f9f1e37eecef303146df477b1a35fea0029024
Brandon Lee <blee2125@gmail.com>
2022-11-27 18:18:34 -0700

    create component to display payment list

---
+++ src/components/Payment/Payments.js
@@ -0,0 +1,24 @@
+import React, {useEffect} from "react";
+import { connect, useSelector } from "react-redux";
+import { getAllPayments } from "../../reducers/PaymentReducer"
+import PaymentList from "./List/PaymentList";
+
+function Leases(props) {
+    const paymentArray = useSelector((state) => state.paymentState.paymentArray)
+
+    useEffect(() => {
+        props.getAllPayments()
+        // eslint-disable-next-line
+    }, [])
+
+    return (
+        <div>
+            Payments
+            <PaymentList
+                paymentArray={paymentArray}
+            />
+        </div>
+    )
+}
+
+export default connect(null, { getAllPayments }) (Leases)
