403cfc37c5c9aead57a11dbec6712cfee3c641e0
Brandon Lee <blee2125@gmail.com>
2022-12-01 20:39:59 -0700

    reformat phone number for table

--- src/components/Customer/List/CustomerListItem.js
+++ src/components/Customer/List/CustomerListItem.js
@@ -10,11 +10,18 @@ function CustomerListItem(props) {
         navigate(path, {state: {customerToView}})
     }
 
+    const formatPhone = () => {
+        const phone = props.customer.phone
+        const p = phone.split('')
+        const reformat = '('+p[0]+p[1]+p[2]+') '+p[3]+p[4]+p[5]+'-'+p[6]+p[7]+p[8]+p[9]
+        return reformat
+    }
+
     return (
         <tr onClick={()=> viewCustomer()}>
             <td>{props.customer.name}</td>
             <td>{props.customer.email}</td>
-            <td>{props.customer.phone}</td>
+            <td>{formatPhone()}</td>
         </tr>
     );
 }
