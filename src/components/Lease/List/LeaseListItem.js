1d8829c4ca16e89e2f1ad6dc88d386be3c34a417
Brandon Lee <blee2125@gmail.com>
2022-12-03 13:43:37 -0700

    add balance to replace total cost

--- src/components/Lease/List/LeaseListItem.js
+++ src/components/Lease/List/LeaseListItem.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import DateFunc from '../../../functions/DateFunc'
@@ -7,6 +7,7 @@ function LeaseListItem(props) {
     const customer = useSelector((state) => state.customerState.customerArray.filter(c => c._id === props.lease.customerId)[0])
     const unit = useSelector((state) => state.storageUnitState.storageUnitArray.filter(u => u._id === props.lease.unitId)[0])
     let navigate = useNavigate();
+    const [balance, setBalance] = useState()
 
     const viewLease = () => {
         let path = `view/${props.lease._id}`
@@ -15,12 +16,27 @@ function LeaseListItem(props) {
 
     const leaseLength = DateFunc.leaseLength(props.lease.startDate, props.lease.endDate)
 
+    function calcBalance() {
+        if (props.lease) {
+            let bal = props.lease.totalCost
+            if (props.lease.payments > 0) {
+                bal = bal - props.lease.payments
+            }
+            setBalance(bal)
+        }
+    }
+
+    useEffect(() => {
+        calcBalance()
+        // eslint-disable-next-line
+    }, [])
+
     return (
         <tr onClick={()=> viewLease()}>
             <td>{unit ? unit.unitNumber : ''}</td>
             <td>{customer ? customer.name : ''}</td>
             <td>{props.lease.rate}</td>
-            <td>{props.lease.totalCost}</td>
+            <td>{balance ? Number(balance).toFixed(2) : ''}</td>
             <td>{props.lease.startDate}</td>
             <td>{props.lease.endDate}</td>
             <td>{leaseLength}</td>
