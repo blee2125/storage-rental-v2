0c9d49238d04189a6a94b9b25b0b9307e7e68536
Brandon Lee <blee2125@gmail.com>
2022-12-03 17:12:54 -0700

    add notifications to store

--- src/store.js
+++ src/store.js
@@ -3,12 +3,14 @@ import storageUnitReducer from './reducers/StorageUnitReducer'
 import customerReducer from './reducers/CustomerReducer'
 import leaseReducer from './reducers/LeaseReducer'
 import PaymentReducer from './reducers/PaymentReducer'
+import NotificationReducer from './reducers/NotificationReducer'
 
 export default configureStore({
   reducer: {
     storageUnitState: storageUnitReducer,
     customerState: customerReducer,
     leaseState: leaseReducer,
-    paymentState: PaymentReducer
+    paymentState: PaymentReducer,
+    notificationState: NotificationReducer
   },
 })
